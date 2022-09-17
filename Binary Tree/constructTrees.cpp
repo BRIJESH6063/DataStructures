49fc26784fd97f4048999af70f5e326049f9879c
BRIJESH6063 <=>
2022-09-17 14:56:13 +0530

    construct BTree commit

--- Binary Tree/constructTrees.cpp
+++ Binary Tree/constructTrees.cpp
@@ -6,16 +6,47 @@ using namespace std ;
 int main()
 {
     int n ; cin >> n ;
-    vector<int> inorder(n) ; vector<int> preorder(n) ;
-    for(int i=0; i<n; i++) cin >> inorder[i] ;
-    for(int i=0; i<n; i++) cin >> preorder[i] ;
+    // vector<int> inorder(n) ; vector<int> pretorder(n) ;
+    // for(int i=0; i<n; i++) cin >> inorder[i] ;
+    // for(int i=0; i<n; i++) cin >> pretorder[i] ;
 
-    BTNode<int>* root = buildTree(inorder, preorder) ;
+    vector<int> inorder(n) ; vector<int> postorder(n) ;
+    for(int i=0; i<n; i++) cin >> inorder[i] ;
+    for(int i=0; i<n; i++) cin >> postorder[i] ;
+
+    // BTNode<int>* root = buildTree(inorder, preorder) ;
+    BTNode<int>* root = buildTree2(inorder, postorder) ;
 
     vector<vector<int>> ans = levelOrder(root) ;
     for(auto x : ans){
         for(auto y : x) cout << y << "  " ;
         cout << endl ;
     }
+
+    cout << "Diameter of tree : " << diameterTree(root) << endl ;
+    pair<int, int> p = diameterOptimised(root) ;
+    cout << "Height : " << p.first << " Diameter : " << p.second << endl ;
+
+    // fiding path to node 
+    vector<int> v ;
+    if(getPath(root, 12, v)){
+        cout << "Path is : " ;
+        for(auto x : v) cout << x << " " ;
+        cout << endl ;
+    }
+    else cout << "Path not available." << endl ;
+
     return 0 ;
 }
+
+/* 
+   input inorder postorder 
+   7
+   4 2 5 1 6 3 7
+   4 5 2 6 7 3 1
+*/
+
+
+/*
+        
+*/
--- Binary Tree/constructTrees.h
+++ Binary Tree/constructTrees.h
@@ -36,6 +36,39 @@ BTNode<int>* buildTree(vector<int> inorder, vector<int> preorder)
 }
 
 
+BTNode<int>* buildTreeHelper2(vector<int> inorder, vector<int> postorder, int inS, int inE, int posS, int posE)
+{
+    if(inS > inE) return NULL ;
+    int data = postorder[posE] ;
+
+    int LeftInS = inS ;
+    int rootIndex = -1 ;
+    for(int i=0; i<inorder.size(); i++){
+        if(inorder[i] == data) rootIndex = i ;
+    }
+    int LeftInE  = rootIndex - 1; 
+    int LeftPosS = posS ; 
+    // leftInE - leftInS = leftPreE - leftPreS 
+    int LeftPosE = LeftPosS + LeftInE - LeftInS ; 
+
+    int RightInS  = rootIndex + 1 ; 
+    int RightInE  = inE; 
+    int RightPosS = LeftPosE + 1 ; 
+    int RightPosE = posE - 1 ; 
+
+    BTNode<int>* root = new BTNode<int>(data) ;
+    root->left  = buildTreeHelper2(inorder, postorder, LeftInS, LeftInE, LeftPosS, LeftPosE) ;
+    root->right = buildTreeHelper2(inorder, postorder, RightInS, RightInE, RightPosS, RightPosE) ;
+
+    return root ;
+}
+BTNode<int>* buildTree2(vector<int> inorder, vector<int> postorder)
+{
+    int n = inorder.size() ;
+    return buildTreeHelper2(inorder, postorder, 0, n-1, 0, n-1) ;
+}
+
+
 vector<vector<int>> levelOrder(BTNode<int>* root)
 {
     vector<vector<int>> v ; vector<int> temp ;
@@ -54,4 +87,54 @@ vector<vector<int>> levelOrder(BTNode<int>* root)
     }
 
     return v ;
+}
+
+int height(BTNode<int>* root){
+    if(root == NULL) return 0 ;
+    return 1 + max(height(root->left), height(root->right)) ;
+}
+
+int diameterTree(BTNode<int>* root){
+    if(! root) return 0 ;
+    int option1 = height(root->left) + height(root->right) ;
+    int option2 = diameterTree(root->left) ;
+    int option3 = diameterTree(root->right) ;
+
+    return max(option1, max(option2, option3)) ;
+}
+
+pair<int, int> heightDiameter(BTNode<int>* root)
+{
+    if(root == NULL){
+        pair<int, int> p = make_pair(0,0) ;
+        return p ;
+    }
+
+    pair<int, int> leftAns = heightDiameter(root->left) ;
+    pair<int, int> rightAns = heightDiameter(root->right) ;
+
+    int lh = leftAns.first ; int ld = leftAns.second ;
+    int rh = rightAns.first ; int rd = rightAns.second ;
+
+    int height = 1 + max(lh, rh) ;
+    int diameter = max(lh+rh, max(ld, rd)) ;
+
+    return make_pair(height, diameter) ;
+}
+pair<int, int> diameterOptimised(BTNode<int>* root){
+    pair<int, int> p = heightDiameter(root) ;
+    return p ;
+}
+
+bool getPath(BTNode<int>* root, int n, vector<int> &v){
+    if(root == NULL) return false ;
+    v.push_back(root->data) ;
+    if(root->data == n) return true ;
+
+    bool leftPath = getPath(root->left, n, v) ;
+    bool rightPath = getPath(root->right, n, v) ;
+
+    if(leftPath || rightPath) return true ;
+    v.pop_back() ; return false ;
+
 }
